586ed6903de324a53c1b55c644d16bc27920b8ef
Xceldeveloper <56355878+Xceldeveloper@users.noreply.github.com>
2020-11-17 21:42:47 +0100

    ''

--- pages/resume.vue
+++ pages/resume.vue
@@ -6,15 +6,58 @@
           <v-spacer></v-spacer>
           <v-btn text small >Download <v-icon right>mdi-download</v-icon></v-btn>
       </v-toolbar>
-      <div id="resume-wrapper">
+      <canvas id="resume-wrapper">
       
-  </div>
+  </canvas>
   </div>
 </template>
 
 <script>
+const pdf = require('pdfjs')
 export default {
+  data(){
+      return{
+        pdf: "http://mozilla.github.io/pdf.js/examples/learning/helloworld.pdf"
+      }
+  },
+   mounted(){
+      this.initPDF();
+   },
+     methods:{
+         initPDF(){
+           // Asynchronous download PDF
+PDFJS.getDocument(this.pdf)
+  .then((pdf)=> {
+    return pdf.getPage(1);
+  })
+  .then((page)=> {
+    // Set scale (zoom) level
+    var scale = 1.5;
 
+    // Get viewport (dimensions)
+    var viewport = page.getViewport(scale);
+
+    // Get canvas#the-canvas
+    var canvas = document.getElementById('resume-wrapper');
+
+    // Fetch canvas' 2d context
+    var context = canvas.getContext('2d');
+
+    // Set dimensions to Canvas
+    canvas.height = viewport.height;
+    canvas.width = viewport.width;
+
+    // Prepare object needed by render method
+    var renderContext = {
+      canvasContext: context,
+      viewport: viewport
+    };
+
+    // Render PDF page
+    page.render(renderContext);
+  })
+         }
+     }
 }
 </script>
 
