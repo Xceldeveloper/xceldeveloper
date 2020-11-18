a0d598119bddf2c3a1006f09ffd909e3b71d1845
Xceldeveloper <56355878+Xceldeveloper@users.noreply.github.com>
2020-11-18 15:02:15 +0100

    resume

--- pages/resume.vue
+++ pages/resume.vue
@@ -1,21 +1,26 @@
 <template>
   <div id="wrapperx">
       <v-toolbar color="transparent" dense>
-          <v-btn icon><v-icon>mdi-chevron-left</v-icon></v-btn>
+          <v-btn icon @click="navBack"><v-icon>mdi-chevron-left</v-icon></v-btn>
           <v-toolbar-title>Resume</v-toolbar-title>
           <v-spacer></v-spacer>
-          <v-btn text small >Download <v-icon right>mdi-download</v-icon></v-btn>
+          <a id="downloader" :href="resume">
+            <v-btn text small ><span v-if="!isMobile">Download</span> <v-icon right>mdi-download</v-icon></v-btn>
+          </a>
+           <v-btn text small @click="$refs.pdf_viewer.print()" ><span v-if="!isMobile">Print</span> <v-icon right>mdi-printer</v-icon></v-btn>
       </v-toolbar>
       <div id="resume-wrapper">
-     <pdf :src="resume"></pdf>
+     <pdf ref="pdf_viewer" :src="resume"></pdf>
   </div>
   </div>
 </template>
 
 <script>
 import pdf from 'vue-pdf'
+import device_mixin from '~/mixins/device_mixin.js'
 
 export default {
+    mixins: [device_mixin],
     components: {
     pdf
   },
@@ -23,6 +28,13 @@ export default {
       return{
         resume:require("~/static/resumex.pdf")
       }
+  },
+  mounted(){
+     var dwnloader = document.getElementById('downloader');
+     dwnloader.download = "overcomeremiator_resume"
+  },
+  methods:{
+
   }
 }
 </script>
@@ -39,5 +51,9 @@ export default {
     background-color: #fff;
     color: #000;
     overflow: auto;
+}
+
+a{
+  text-decoration: none;
 }
 </style>
