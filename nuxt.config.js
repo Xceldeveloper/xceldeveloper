2126d2adf90ad646c898e188274f7592fca60135
Xceldeveloper <56355878+Xceldeveloper@users.noreply.github.com>
2020-12-16 07:57:19 +0100

    new

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,7 @@ export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     titleTemplate: '%s - xceldeveloper',
-    title: 'Fullstack developer',
+    title: 'Overcomer Emiator - Fullstack developer',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
