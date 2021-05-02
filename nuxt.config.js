fe861b6c5f7e81a4879abaa48e0e312b21c5a777
Xceldeveloper <56355878+Xceldeveloper@users.noreply.github.com>
2021-05-02 20:51:04 +0100

    bravo on the way

---
+++ nuxt.config.js
@@ -0,0 +1,118 @@
+
+import colors from 'vuetify/es5/util/colors'
+
+export default {
+  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
+  ssr: false,
+
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'xceldeveloper',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+     '~/assets/icons/materialdesignicons.min.css',
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+    { src: '~/plugins/aos.js', mode: 'client' }
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/tailwindcss
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/vuetify'
+  ],
+
+  tailwindcss:{
+    viewer:true
+  },
+
+  // Modules: https://goC:\Users\Xceldeveloper\Documents\PROJECTS\XCELDEV\frontend .nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+    // https://go.nuxtjs.dev/pwa
+    '@nuxtjs/pwa',
+    // https://go.nuxtjs.dev/content
+    '@nuxt/content',
+    [
+      '@nuxtjs/firebase',
+      {
+        config: {
+          apiKey: "AIzaSyAtcGelZ0OOFvAiTCyc4iSIAP02TLSbTsc",
+          authDomain: "xceldeveloper-app.firebaseapp.com",
+          projectId: "xceldeveloper-app",
+          storageBucket: "xceldeveloper-app.appspot.com",
+          messagingSenderId: "789069451228",
+          appId: "1:789069451228:web:ffeda2a912eddafa504919",
+          measurementId: "G-8Z75SHZL4M"
+        },
+        services: {
+          auth: true, // Just as example. Can be any other service.
+          analytics:true
+        }
+      }
+    ]
+  ],
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {},
+
+  // PWA module configuration: https://go.nuxtjs.dev/pwa
+  pwa: {
+    manifest: {
+      lang: 'en'
+    }
+  },
+  vuetify: {
+    customVariables: ['~/assets/variables.scss'],
+    theme: {
+      dark: true,
+      themes: {
+        dark: {
+          primary: colors.blue.darken2,
+          accent: colors.grey.darken1,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3
+        }
+      }
+    }
+  },
+
+  // Content module configuration: https://go.nuxtjs.dev/config-content
+  content: {},
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+    extend(config) {
+      // Find the rule which contains a assets file extension
+      const assetsLoader = config.module.rules.find(rule => rule.test.test('.png'));
+
+      // Overwrite the test regex and add `pdf`
+      assetsLoader.test = /\.(png|jpe?g|gif|svg|webp|pdf)$/i;
+
+      return config;
+    },
+  }
+}
