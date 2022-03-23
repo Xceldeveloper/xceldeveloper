13b9d2c812699c73c3df2b8661b4f75c064df814
Xcedeveloper <xceldeveloper@gmail.com>
2022-03-23 03:30:11 +0100

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,35 +1,34 @@
-
-import colors from 'vuetify/es5/util/colors'
+import colors from "vuetify/es5/util/colors";
 
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
 
   // Target: https://go.nuxtjs.dev/config-target
-  target: 'static',
+  target: "static",
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'xceldeveloper',
+    title: "xceldeveloper - Overcomer Emiator",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "Overcomer Emiator - A passionate programmer",
+        name: "Overcomer Emiator - A passionate programmer",
+        content: "Overcomer Emiator - A passionate programmer",
+      },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/icon.jpg" }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-     '~/assets/icons/materialdesignicons.min.css',
-  ],
+  css: ["~/assets/icons/materialdesignicons.min.css", '@/assets/css/main.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    { src: '~/plugins/aos.js', mode: 'client' },
-    { src: '~/plugins/typer.js', mode: 'client' }
+    { src: "~/plugins/aos.js", mode: "client" },
+    { src: "~/plugins/typer.js", mode: "client" },
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
@@ -38,24 +37,24 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/tailwindcss
-    '@nuxtjs/tailwindcss',
-    '@nuxtjs/vuetify'
+    '@nuxt/postcss8',
+    "@nuxtjs/vuetify",
   ],
 
-  tailwindcss:{
-    viewer:true
+  tailwindcss: {
+    viewer: true,
   },
 
   // Modules: https://goC:\Users\Xceldeveloper\Documents\PROJECTS\XCELDEV\frontend .nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
+    "@nuxtjs/axios",
     // https://go.nuxtjs.dev/pwa
-    '@nuxtjs/pwa',
+    "@nuxtjs/pwa",
     // https://go.nuxtjs.dev/content
-    '@nuxt/content',
+    "@nuxt/content",
     [
-      '@nuxtjs/firebase',
+      "@nuxtjs/firebase",
       {
         config: {
           apiKey: "AIzaSyAtcGelZ0OOFvAiTCyc4iSIAP02TLSbTsc",
@@ -64,14 +63,14 @@ export default {
           storageBucket: "xceldeveloper-app.appspot.com",
           messagingSenderId: "789069451228",
           appId: "1:789069451228:web:ffeda2a912eddafa504919",
-          measurementId: "G-8Z75SHZL4M"
+          measurementId: "G-8Z75SHZL4M",
         },
         services: {
           auth: true, // Just as example. Can be any other service.
-          analytics:true
-        }
-      }
-    ]
+          analytics: true,
+        },
+      },
+    ],
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
@@ -80,11 +79,11 @@ export default {
   // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
     manifest: {
-      lang: 'en'
-    }
+      lang: "en",
+    },
   },
   vuetify: {
-    customVariables: ['~/assets/variables.scss'],
+    customVariables: ["~/assets/variables.scss"],
     theme: {
       dark: true,
       themes: {
@@ -95,10 +94,10 @@ export default {
           info: colors.teal.lighten1,
           warning: colors.amber.base,
           error: colors.deepOrange.accent4,
-          success: colors.green.accent3
-        }
-      }
-    }
+          success: colors.green.accent3,
+        },
+      },
+    },
   },
 
   // Content module configuration: https://go.nuxtjs.dev/config-content
@@ -108,12 +107,21 @@ export default {
   build: {
     extend(config) {
       // Find the rule which contains a assets file extension
-      const assetsLoader = config.module.rules.find(rule => rule.test.test('.png'));
+      const assetsLoader = config.module.rules.find((rule) =>
+        rule.test.test(".png")
+      );
 
       // Overwrite the test regex and add `pdf`
       assetsLoader.test = /\.(png|jpe?g|gif|svg|webp|pdf)$/i;
 
       return config;
     },
-  }
-}
+    postcss: {
+      plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+      },
+    },
+
+  },
+};
--- pages/index.vue
+++ pages/index.vue
@@ -1,121 +1,191 @@
 <template>
-  <div class="wrapper">
-    
-    <app-header :focus="focusHeader ? false : true" :views="views" />
-    <main-cover @hidden="canFocus"/>
-     <div id="over-lap">
-            <div id="over-wrap">
-               <div id="badges" class=" grid grid-cols-3">
-                 <div class="badge-class">
-                   <div style="width:100%;text-align:center;display:block"><v-icon>mdi-web</v-icon> </div> <br>
-                   <div>Web Developer</div>
-                   </div>
-                 <div class="badge-class ">App Developer <v-icon>mdi-app</v-icon>
 
-                 </div>
-                 <div class="badge-class">
-                    <span><v-icon>mdi-git</v-icon></span> <br>
-                  <span> Open Source Contributor</span></div>
-               </div>
-            </div>
-          </div>
+  <div class="page p-14 gap-10 grid grid-cols-2">
 
-                <about @visible="whichView('about',$event)"/>
-                <br>
-                <project @visible="whichView('project',$event)"/>
-        
+    <div class="flex items-center">
 
-          
+      <div class="w-full">
 
+        <img class="w-52 h-52 rounded-full logo" src="/icon.jpg" alt="" />
 
+        <span class="block text-white text-4xl mt-5">Overcomer Emiator</span>
 
- 
+        <span class="text-gray-400 block font-semibold"> {{dd}} </span>
 
-    <div id="box"></div>
+        <span class="block tracking-wider text-left text-gray-400 my-5">
+
+          <vue-typed-js
+            :startDelay="3000"
+            :typeSpeed="30"
+            cursorChar=" _"
+            :strings="[
+              `  Hi, I am a passionate developer with good experience designing Webapps,
+          Blogs, Android Apps, Opensource Projects etc utilizing varieties of Software
+          Technologies which include Vuejs, Nuxt, Flutter, Nodejs, Django, JQuery,
+          Tailwindcss, Scss, Git I have experience working remotely building meaningful
+          and productive software with also the ability to work with minimal supervision
+          as I have the zeal to tackle any problems I might encounter independently
+          to achieve the desired result`,
+            ]"
+          >
+
+            <span class="typing" id="quote-x"></span>
+
+          </vue-typed-js>
+
+        </span>
+
+        <div class="">
+
+          <a href="https://twitter.com/xceldeveloper" target="_blank">
+
+            <v-btn icon>
+
+              <v-icon>mdi-twitter</v-icon>
+
+            </v-btn>
+
+          </a>
+
+          <a
+            href="https://www.linkedin.com/in/overcomer-emiator-5573141b2"
+            target="_blank"
+          >
+
+            <v-btn icon>
+
+              <v-icon>mdi-linkedin</v-icon>
+
+            </v-btn>
+
+          </a>
+
+          <a href="mailto:xceldeveloper@gmail.com" target="_blank">
+
+            <v-btn icon>
+
+              <v-icon>mdi-gmail</v-icon>
+
+            </v-btn>
+
+          </a>
+
+          <a href="https://github.com/xceldeveloper" target="_blank">
+
+            <v-btn icon>
+
+              <v-icon>mdi-github</v-icon>
+
+            </v-btn>
+
+          </a>
+
+        </div>
+
+      </div>
+
+    </div>
+
+    <div>
+
+      <div class="grid grid-cols-3 gap-10">
+
+        <v-btn
+          large
+          text
+          @click="tab = 0"
+          :color="tab != 0 ? 'grey' : '#fff'"
+          elevation="0"
+          rounded
+        >
+
+          <v-icon>mdi-account</v-icon>
+
+        </v-btn>
+
+        <v-btn
+          large
+          outlined
+          elevation="0"
+          @click="tab = 1"
+          :color="tab != 1 ? 'grey' : '#fff'"
+          rounded
+        >
+           Work
+        </v-btn>
+
+        <a
+          href="http://https://my.indeed.com/p/overcomere-my85kbn"
+          target="_blank"
+        >
+
+          <v-btn text elevation="0" color="grey" rounded large>
+             Resume
+            <v-icon right>mdi-arrow-top-right-thick</v-icon>
+
+          </v-btn>
+
+        </a>
+
+      </div>
+
+    </div>
+
   </div>
+
 </template>
 
 <script>
-import about from '~/components/about.vue';
-import project from '~/components/projects.vue';
-import appHeader from '~/components/header.vue';
-import mainCover from '~/components/main-cover.vue';
-
 export default {
-  components:{appHeader,mainCover,about,project},
   data() {
     return {
-      focusHeader:false,
-      views:{
-        about:false,
-        project:false,
-        resume:false
-      }
+      dd: "<A Passionate Programmer/>",
+      tab: 0,
     };
   },
-  methods:{
-    canFocus(val){
-      this.focusHeader = val;
-    },
-    whichView(view,res){
-     switch(view){
-       case 'about':
-         this.views.about = res;
-         break;
-       case 'project':
-         this.views.project = res;
-         break;
-     }
-    }
-  }
+  methods: {},
 };
 </script>
 
-<style>
-
-*{
-  color:#fff;
-}
-.wrapper{
+<style lang="scss" scoped>
+.page {
   width: 100vw;
-  height: 100vh;
-  overflow: auto;
+  min-height: 100vh;
+  background-color: #131111;
 }
 
-#over-lap {
-  width: 100%;
-  position: relative;
-  height: 120px;
-  z-index: 100;
+.logo {
+  padding: 5px;
+  box-shadow: 1px 0px 10px #fff;
+  animation: avataranim 4s infinite;
+  -webkit-animation: avataranim 4s infinite;
+  /* Safari 4+ */
+  -moz-animation: avataranim 4s infinite;
+  /* Fx 5+ */
+  -o-animation: avataranim 4s infinite;
 }
 
-#over-wrap{
- position: absolute;
-  top: -60px;
-  height: 120px;
-  width: 100%;
+@keyframes avataranim {
+  0% {
+    box-shadow: 0px 1px 8px rgba(255, 255, 255, 0.8);
+  }
+
+  30% {
+    box-shadow: 0px 1px 18px rgba(255, 255, 255, 0.7);
+  }
+
+  70% {
+    box-shadow: 0px 1px 5px rgba(255, 255, 255, 0.8);
+  }
+
+  100% {
+    box-shadow: 0px 1px 18px rgba(255, 255, 255, 0.8);
+  }
 }
 
-#badges{
-  margin: auto;
-  height: 100%;
-  border-radius: 80px;
-  background-color: #000;
-  width: 60%;
-  max-width: 800px;
-  box-shadow: 0px 0px 10px #000;
-  visibility: hidden;
-}
-
-
-#box{
-  height: 200vh;
-  width: 100%;
-}
-
-.badge-class{
-  text-align: center;
-  height: 100%;
-  line-height: 20px;
+#quote-x {
+  font-size: 15px;
+  display: block;
 }
 </style>
+
